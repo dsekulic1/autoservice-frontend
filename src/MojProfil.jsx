5cf304e1afbf4601d9eb13e693d74fb9bd01ed02
Davor <dsekulic1@etf.unsa.ba>
2022-05-07 20:39:59 +0200

    add

--- src/MojProfil.jsx
+++ src/MojProfil.jsx
@@ -1,67 +1,131 @@
-import React from "react";
-import FormControl from '@material-ui/core/FormControl';
-import Box from "@material-ui/core/Box";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
+import React from 'react'
+import FormControl from '@material-ui/core/FormControl'
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 
 const MojProfil = () => {
-  return <div style={{ backgroundColor: 'black', display: 'flex', justifyContent: 'center', height: '100vh', flexDirection: 'column', width: '100%', }} >
-  <Box fullWidth style={{marginLeft:'2%', borderBottom:' 1px solid', borderColor: '#ffff', height:'5vh' }}  >
-      <p  style={{ width:"10vh", color:'#ffff',borderBottom:' 5px solid', borderColor: 'red' }}  value={'Mojprofil'}
-        >Moj Profil</p>
-</Box>
-<Box  style={{ backgroundColor: 'black', display: 'flex', justifyContent: 'center', height: '100%', width: '100%'}}>
-  <div>
-<FormControl style={{  width: '60vh', marginTop:'5%' }}>
-  <TextField 
-          id="outlined-read-only-input"
-          label="Ime"
-          defaultValue="Mirnesa"
-          InputProps={{
-            readOnly: true,
+  return (
+    <div
+      style={{
+        backgroundColor: 'black',
+        display: 'flex',
+        justifyContent: 'center',
+        height: '100vh',
+        flexDirection: 'column',
+        width: '100%',
+      }}
+    >
+      <Box
+        fullWidth
+        style={{
+          marginLeft: '2%',
+          borderBottom: ' 1px solid',
+          borderColor: '#ffff',
+          height: '5vh',
+        }}
+      >
+        <p
+          style={{
+            width: '10vh',
+            color: '#ffff',
+            borderBottom: ' 5px solid',
+            borderColor: 'red',
           }}
-          style={{ backgroundColor:'#a7a3a3', marginTop:'4%', padding:'1%'}}
-          focused 
-          sx={{ height:'50vh'}}
-        />
-          <TextField 
-          id="outlined-read-only-input"
-          label="Prezime"
-          defaultValue="Salihovic"
-          InputProps={{
-            readOnly: true,
-          }}
-          style={{ backgroundColor:'#a7a3a3', marginTop:'4%', padding:'1%'}}
-          focused 
-          sx={{ height:'50vh'}}
-        />
-                  <TextField 
-          id="outlined-read-only-input"
-          label="Email"
-          defaultValue="email@hotmail.com"
-          InputProps={{
-            readOnly: true,
-          }}
-          style={{ backgroundColor:'#a7a3a3', marginTop:'4%', padding:'1%'}}
-          
-          sx={{ height:'50vh'}}
-        />
-  
-   <TextField 
-          id="outlined-input"
-          label="Mobitel"
-          type='phone'
-          defaultValue="000-000-000"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          style={{ backgroundColor:'#a7a3a3', marginTop:'4%', padding:'2%' }}
-          focused 
-          sx={{ height:'50vh'}}
-        />
-<Button variant="contained" style={{backgroundColor: '#AE2331',marginTop:'10%', marginLeft:'70%', color:'#ffff', borderRadius:'10'}}>UREDI</Button>
-</FormControl>
-</div>
-</Box>
-</div>
-};
+          value={'Mojprofil'}
+        >
+          Moj Profil
+        </p>
+      </Box>
+      <Box
+        style={{
+          backgroundColor: 'black',
+          display: 'flex',
+          justifyContent: 'center',
+          height: '100%',
+          width: '100%',
+        }}
+      >
+        <div>
+          <FormControl style={{ width: '60vh', marginTop: '5%' }}>
+            <TextField
+              id='outlined-read-only-input'
+              label='Ime'
+              defaultValue='Mirnesa'
+              InputProps={{
+                readOnly: true,
+              }}
+              style={{
+                backgroundColor: '#a7a3a3',
+                marginTop: '4%',
+                padding: '1%',
+              }}
+              focused
+              sx={{ height: '50vh' }}
+            />
+            <TextField
+              id='outlined-read-only-input'
+              label='Prezime'
+              defaultValue='Salihovic'
+              InputProps={{
+                readOnly: true,
+              }}
+              style={{
+                backgroundColor: '#a7a3a3',
+                marginTop: '4%',
+                padding: '1%',
+              }}
+              focused
+              sx={{ height: '50vh' }}
+            />
+            <TextField
+              id='outlined-read-only-input'
+              label='Email'
+              defaultValue='email@hotmail.com'
+              InputProps={{
+                readOnly: true,
+              }}
+              style={{
+                backgroundColor: '#a7a3a3',
+                marginTop: '4%',
+                padding: '1%',
+              }}
+              sx={{ height: '50vh' }}
+            />
+
+            <TextField
+              id='outlined-input'
+              label='Mobitel'
+              type='phone'
+              defaultValue='000-000-000'
+              InputLabelProps={{
+                shrink: true,
+              }}
+              style={{
+                backgroundColor: '#a7a3a3',
+                marginTop: '4%',
+                padding: '2%',
+              }}
+              focused
+              sx={{ height: '50vh' }}
+            />
+            <Button
+              variant='contained'
+              style={{
+                backgroundColor: '#AE2331',
+                marginTop: '10%',
+                marginLeft: '70%',
+                color: '#ffff',
+                borderRadius: '10',
+              }}
+            >
+              UREDI
+            </Button>
+          </FormControl>
+        </div>
+      </Box>
+    </div>
+  )
+}
+
+export default MojProfil
--- src/NoviZahtjev.jsx
+++ src/NoviZahtjev.jsx
@@ -1,9 +1,8 @@
-
-import React from "react";
-import Box from "@material-ui/core/Box";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import MenuItem from "@material-ui/core/MenuItem";
+import React from 'react'
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import MenuItem from '@material-ui/core/MenuItem'
 
 const kategorije = [
   {
@@ -22,7 +21,7 @@ const kategorije = [
     value: 'kategorija 4',
     label: 'D',
   },
-];
+]
 
 const tipovi = [
   {
@@ -41,96 +40,181 @@ const tipovi = [
     value: 'Tip 4',
     label: 'D',
   },
-];
+]
 
-const NoviZahtjev = props => {
-  const [currency, setCurrency] = React.useState('EUR');
+const NoviZahtjev = (props) => {
+  const [currency, setCurrency] = React.useState('EUR')
 
- // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
- //   setCurrency(event.target.value);
+  // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setCurrency(event.target.value);
   //};;
 
-  return <div style={{ backgroundColor: 'black', display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center', height: '100%', width: '100%'}} >
-  <Box sx={{
-  width: '45%',
-  height: 700,
-  display: 'flex',
-  justifyContent: 'center',
-  backgroundColor: '#ffff',
-  alignItems: 'center'
-}} style={{marginTop:'2%', marginBottom:'2%'}}>   
-<Box
-sx={{
-  width: 500,
-  maxWidth: '100%'
-}}
->
-<h3 fullWidth style={{
-  textAlign: 'center', marginBottom:'2%', height: '4vh', font:'Poppins', fontSize:'30px' 
-}}  >Novi zahtjev</h3>
-<p fullWidth id="fullWidth" style={{
-  textAlign: 'center', marginBottom:'2%', 
-  backgroundColor: '#AE2331', height: '4vh', color:'#ffff'
-}}  value={'ime prezime'}>Ime Orezime</p>
-
-<p fullWidth id="fullWidth" style={{
-  borderColor: 'black', border: '1px solid', height: '4vh'
-}}  value={'grad'}>Sarajevo</p>
-
-<Box
-
-   sx={{display: 'flex', flexDirection: 'row' , justifyContent: 'center'  }} style={{
-    textAlign: 'center',
-     height: '4vh', color:'#ffff'
-     
-  }}
+  return (
+    <div
+      style={{
+        backgroundColor: 'black',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        height: '100%',
+        width: '100%',
+      }}
     >
-      <p id="outlined-basic" label="Outlined" style={{ borderRight:'1px solid', backgroundColor: '#AE2331',  height: '4vh', width:"40vh"}} >datum</p> 
-      <p id="filled-basic" label="Filled" style={{ borderRight:'1px solid', backgroundColor: '#AE2331',  height: '4vh', width:"40vh"}} >sati</p> 
-    </Box>
- <Box sx={{display:'flex', flexDirection: 'column' }}>
-    <TextField style={{marginTop:'3vh'}}
-          id="standard-select-currency"
-          select
-          label="Kategorija"
-          value={currency}
-          onChange={handleChange}
-          helperText="Molimo Vas izaberite kategoriju problema."
-          variant="standard"
+      <Box
+        sx={{
+          width: '45%',
+          height: 700,
+          display: 'flex',
+          justifyContent: 'center',
+          backgroundColor: '#ffff',
+          alignItems: 'center',
+        }}
+        style={{ marginTop: '2%', marginBottom: '2%' }}
+      >
+        <Box
+          sx={{
+            width: 500,
+            maxWidth: '100%',
+          }}
         >
-          {kategorije.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
+          <h3
+            fullWidth
+            style={{
+              textAlign: 'center',
+              marginBottom: '2%',
+              height: '4vh',
+              font: 'Poppins',
+              fontSize: '30px',
+            }}
+          >
+            Novi zahtjev
+          </h3>
+          <p
+            fullWidth
+            id='fullWidth'
+            style={{
+              textAlign: 'center',
+              marginBottom: '2%',
+              backgroundColor: '#AE2331',
+              height: '4vh',
+              color: '#ffff',
+            }}
+            value={'ime prezime'}
+          >
+            Ime Orezime
+          </p>
 
-        <TextField style={{marginTop:'3vh'}}
-          id="standard-select-currency"
-          select
-          label="Tip"
-          value={currency}
-          onChange={handleChange}
-          helperText="Molimo Vas izaberite tip problema"
-          variant="standard"
-        >
-          {tipovi.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-        <TextField  style={{marginTop:'3vh'}}
-          id="standard-multiline-static"
-          label="Opis problema"
-          multiline
-          rows={4}
-          variant="standard"
-        />
-<Button variant="contained" style={{backgroundColor: '#AE2331',marginTop:'10%', marginLeft:'35%', marginRight:'35%', color:'#ffff', borderRadius:'10'}}>Posalji</Button>
-</Box>
-</Box>
-</Box></div>;
-};
+          <p
+            fullWidth
+            id='fullWidth'
+            style={{
+              borderColor: 'black',
+              border: '1px solid',
+              height: '4vh',
+            }}
+            value={'grad'}
+          >
+            Sarajevo
+          </p>
 
-export default NoviZahtjev;
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+            }}
+            style={{
+              textAlign: 'center',
+              height: '4vh',
+              color: '#ffff',
+            }}
+          >
+            <p
+              id='outlined-basic'
+              label='Outlined'
+              style={{
+                borderRight: '1px solid',
+                backgroundColor: '#AE2331',
+                height: '4vh',
+                width: '40vh',
+              }}
+            >
+              datum
+            </p>
+            <p
+              id='filled-basic'
+              label='Filled'
+              style={{
+                borderRight: '1px solid',
+                backgroundColor: '#AE2331',
+                height: '4vh',
+                width: '40vh',
+              }}
+            >
+              sati
+            </p>
+          </Box>
+          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            <TextField
+              style={{ marginTop: '3vh' }}
+              id='standard-select-currency'
+              select
+              label='Kategorija'
+              value={currency}
+              //onChange={handleChange}
+              helperText='Molimo Vas izaberite kategoriju problema.'
+              variant='standard'
+            >
+              {kategorije.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+
+            <TextField
+              style={{ marginTop: '3vh' }}
+              id='standard-select-currency'
+              select
+              label='Tip'
+              value={currency}
+              //onChange={handleChange}
+              helperText='Molimo Vas izaberite tip problema'
+              variant='standard'
+            >
+              {tipovi.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              style={{ marginTop: '3vh' }}
+              id='standard-multiline-static'
+              label='Opis problema'
+              multiline
+              rows={4}
+              variant='standard'
+            />
+            <Button
+              variant='contained'
+              style={{
+                backgroundColor: '#AE2331',
+                marginTop: '10%',
+                marginLeft: '35%',
+                marginRight: '35%',
+                color: '#ffff',
+                borderRadius: '10',
+              }}
+            >
+              Posalji
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+    </div>
+  )
+}
+
+export default NoviZahtjev
